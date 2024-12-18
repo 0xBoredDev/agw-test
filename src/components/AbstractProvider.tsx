d133a4b56023ca01d9070b4b11fd51a7a72469fb
0xBoredDev <caughmanconsulting@gmail.com>
2024-12-18 10:36:34 -0500

    init

---
+++ src/components/AbstractProvider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { AbstractWalletProvider } from "@abstract-foundation/agw-react";
+
+export default function AbstractProvider({children}: {children: React.ReactNode}) {
+    const config = {
+        testnet: true
+    };
+
+    return (
+        <AbstractWalletProvider config={config}>
+            {children}
+        </AbstractWalletProvider>
+    )
+}
